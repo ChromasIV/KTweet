f78c2d3a459ca3b3ffbe1ee5e5166fc5b8793fc1
Thomas Carney <tpctoy@gmail.com>
2021-11-27 22:39:53 -0500

    adding maven repository publish for GitHub

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,12 @@ plugins {
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.serialization") version "1.6.0"
     id("org.jetbrains.dokka") version "1.6.0"
+    id("maven-publish")
+    id("signing")
 }
 
 group = "com.chromasgaming"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -16,15 +18,25 @@ repositories {
 dependencies {
     implementation("org.junit.jupiter:junit-jupiter:5.7.0")
     testImplementation(kotlin("test"))
-    implementation("io.ktor:ktor-server-core:1.6.2")
-    implementation("io.ktor:ktor-server-netty:1.6.2")
     implementation("io.ktor:ktor-client-core:1.6.2")
     implementation("io.ktor:ktor-client-cio:1.6.2")
     implementation("io.ktor:ktor-client-serialization:1.6.2")
     implementation("io.ktor:ktor-client-auth:1.6.2")
     implementation("io.ktor:ktor-client-logging:1.6.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
+}
 
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/ChromasIV/KTweet")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 tasks.test {
