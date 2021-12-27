9970d86fb012eb463ffcc7a728c8c9c7ed243a60
Thomas Carney <tpctoy@gmail.com>
2021-12-26 21:28:41 -0500

    Update TweetObject
    The Tweet object has a long list of ‘root-level’ fields, such as id, text, and created_at.
    Tweet objects are also the ‘parent’ object to several child objects including user, media, poll, and place.
    
    ref: https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/tweet

--- src/main/kotlin/com/chromasgaming/ktweet/dtos/TweetObject.kt
+++ src/main/kotlin/com/chromasgaming/ktweet/dtos/TweetObject.kt
@@ -2,6 +2,7 @@ package com.chromasgaming.ktweet.dtos
 
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonObject
 import java.time.Instant
 
 
@@ -10,12 +11,61 @@ import java.time.Instant
 data class TweetObject(
     val id: String,
     val text: String,
-    val attachments: Attachments? = null,
-    val author_id: String? = null,
-    val context_annotations: List<String>? = null,
-    val conversation_id: String? = null,
-    @Contextual val created_at: Instant? = null
+    val attachments: Attachments?,
+    val author_id: String?,
+    val context_annotations: List<String>?,
+    val conversation_id: String?,
+    @Contextual val created_at: Instant?,
+    val entities: JsonObject?,
+    val geo: Geo?,
+    val in_reply_to_user_id: String?,
+    val lang: String?,
+    val non_public_metrics: NonPublicMetrics?,
+    val organic_metrics: OrganicMetrics?,
+    val possibly_sensitive: Boolean?,
+    val promoted_metrics: PromotedMetrics?,
+    val public_metrics: PublicMetrics?,
+    val referenced_tweets: ArrayList<JsonObject>?,
+    val reply_settings: String?,
+    val source: String?,
+    val withheld: WithHolding?
+
 )
 
 @Serializable
-data class Attachments(val poll_ids: ArrayList<String>? = null, val media_keys: ArrayList<String>? = null)
+data class Attachments(val poll_ids: ArrayList<String>?, val media_keys: ArrayList<String>?)
+
+@Serializable
+data class Geo(val coordinates: Coordinates?, val place_id: String?)
+
+@Serializable
+data class Coordinates(val type: String?, val coordinates: ArrayList<Float>?)
+
+@Serializable
+data class NonPublicMetrics(val impression_count: Int?, val url_link_clicks: Int?, val user_profile_clicks: Int?)
+
+@Serializable
+data class OrganicMetrics(
+    val impression_count: Int?,
+    val like_count: Int?,
+    val reply_count: Int?,
+    val retweet_count: Int?,
+    val url_link_clicks: Int?,
+    val user_profile_clicks: Int?
+)
+
+@Serializable
+data class PromotedMetrics(
+    val impression_count: Int?,
+    val like_count: Int?,
+    val reply_count: Int?,
+    val retweet_count: Int?,
+    val url_link_clicks: Int?,
+    val user_profile_clicks: Int?
+)
+
+@Serializable
+data class PublicMetrics(val retweet_count: Int?, val reply_count: Int?, val like_count: Int?, val quote_count: Int?)
+
+@Serializable
+data class WithHolding(val copyright: Boolean?, val country_codes: ArrayList<String>?)
--- src/test/kotlin/com/chromasgaming/ktweet/tweets/SearchTweetsTest.kt
+++ src/test/kotlin/com/chromasgaming/ktweet/tweets/SearchTweetsTest.kt
@@ -19,11 +19,59 @@ internal class SearchTweetsTest {
     @Test
     fun searchTweets(): Unit = runBlocking {
         val paramMap = LinkedHashMap<String, String>()
-        paramMap["query"] = "from:chromasiv -is:retweet"
+        paramMap["query"] = "from:chromasiv -is:retweet has:media"
         paramMap["tweet.fields"] =
             "created_at,attachments"
         val tweetObject = searchTweets.search(paramMap)
-        //print(json.decodeFromString<JsonObject>(searchTweets.search(paramMap))["data"])
+        print(tweetObject)
+    }
+
+    @Test
+    fun searchTweets_details(): Unit = runBlocking {
+        val paramMap = LinkedHashMap<String, String>()
+        paramMap["query"] = "from:chromasiv -is:retweet"
+        paramMap["tweet.fields"] =
+            "author_id,context_annotations,entities"
+        val tweetObject = searchTweets.search(paramMap)
+        print(tweetObject)
+    }
+
+    @Test
+    fun searchTweets_geo(): Unit = runBlocking {
+        val paramMap = LinkedHashMap<String, String>()
+        paramMap["query"] = "from:chromasiv -is:retweet"
+        paramMap["tweet.fields"] =
+            "geo"
+        val tweetObject = searchTweets.search(paramMap)
+        print(tweetObject)
+    }
+
+    @Test
+    fun searchTweets_metrics(): Unit = runBlocking {
+        val paramMap = LinkedHashMap<String, String>()
+        paramMap["query"] = "from:chromasiv"
+        paramMap["tweet.fields"] = "non_public_metrics,public_metrics,organic_metrics"
+        //Uncomment if you have promoted metrics
+        //paramMap["tweet.fields"] = "non_public_metrics,public_metrics,organic_metrics,promoted_metrics"
+        val tweetObject = searchTweets.search(paramMap)
+        print(tweetObject)
+    }
+
+    @Test
+    fun searchTweets_reference(): Unit = runBlocking {
+        val paramMap = LinkedHashMap<String, String>()
+        paramMap["query"] = "from:chromasiv"
+        paramMap["tweet.fields"] = "in_reply_to_user_id,referenced_tweets"
+        val tweetObject = searchTweets.search(paramMap)
+        print(tweetObject)
+    }
+
+    @Test
+    fun searchTweets_others(): Unit = runBlocking {
+        val paramMap = LinkedHashMap<String, String>()
+        paramMap["query"] = "from:chromasiv"
+        paramMap["tweet.fields"] = "reply_settings,source,withheld"
+        val tweetObject = searchTweets.search(paramMap)
         print(tweetObject)
     }
 
