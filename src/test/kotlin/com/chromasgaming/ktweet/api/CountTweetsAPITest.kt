b7014a90c05ec327d8979de6225e2539ca941d04
Thomas Carney <tpctoy@gmail.com>
2023-03-14 22:28:45 -0400

    Added import statement for ClientConfig Replaced runBlocking with runTest Created an instance of ClientConfig Passed clientConfig to CountTweetsAPI constructor Modified recentTweets function to use runTest instead of runBlocking Updated paramMap to use LinkedHashMap instead of HashMap

--- src/test/kotlin/com/chromasgaming/ktweet/api/CountTweetsAPITest.kt
+++ src/test/kotlin/com/chromasgaming/ktweet/api/CountTweetsAPITest.kt
@@ -1,22 +1,23 @@
 package com.chromasgaming.ktweet.api
 
+import com.chromasgaming.ktweet.config.ClientConfig
 import com.chromasgaming.ktweet.models.Granularity
 import com.chromasgaming.ktweet.oauth2.TwitterOauth2Authentication
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class CountTweetsAPITest {
     private lateinit var countTweetsAPI: CountTweetsAPI
-
+    private val clientConfig = ClientConfig()
 
     @BeforeEach
     fun setUp() {
-        countTweetsAPI = CountTweetsAPI()
+        countTweetsAPI = CountTweetsAPI(clientConfig)
     }
 
     @Test
-    fun recentTweets(): Unit = runBlocking {
+    fun recentTweets() = runTest {
         val paramMap = LinkedHashMap<String, String>()
         paramMap["query"] = "from:250181762"
         paramMap["granularity"] = Granularity.DAY.value
