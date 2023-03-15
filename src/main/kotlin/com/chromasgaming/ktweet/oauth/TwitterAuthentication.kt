b458f57a8655466e66a7d31cea097d270d7ce63d
Thomas Carney <tpctoy@gmail.com>
2023-03-14 22:22:40 -0400

    Remove unused constants Remove unused imports Create a client property in the TwitterAuthentication class to be used throughout the class Update getRequestToken and getAccessToken functions to use execute instead of post on the client Refactor getRequestToken to extract the token values into a separate function getTokenValue Refactor getAccessToken to extract the creation of the authorization header string into a separate function createAuthorizationHeaderString and use the function in both getRequestToken and getAccessToken Remove the unnecessary assignment of the Authorization header value to a variable in both getRequestToken and getAccessToken Update getAccessToken to extract the token values using getTokenValue instead of substring operations

--- src/main/kotlin/com/chromasgaming/ktweet/oauth/TwitterAuthentication.kt
+++ src/main/kotlin/com/chromasgaming/ktweet/oauth/TwitterAuthentication.kt
@@ -2,11 +2,6 @@ package com.chromasgaming.ktweet.oauth
 
 import com.chromasgaming.ktweet.config.ClientConfig
 import com.chromasgaming.ktweet.constants.BASEURL
-import com.chromasgaming.ktweet.constants.NINE
-import com.chromasgaming.ktweet.constants.THIRTEEN
-import com.chromasgaming.ktweet.constants.TWELVE
-import com.chromasgaming.ktweet.constants.TWENTY
-import com.chromasgaming.ktweet.constants.TWENTYSIX
 import com.chromasgaming.ktweet.models.AccessToken
 import com.chromasgaming.ktweet.models.RequestToken
 import io.ktor.client.call.body
@@ -14,43 +9,30 @@ import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 
 class TwitterAuthentication {
 
+    private val client = ClientConfig()
+
     suspend fun getRequestToken(consumerKey: String, consumerSecret: String): RequestToken {
-        val client = ClientConfig()
         val builder = HttpRequestBuilder()
-
-        val signatureBuilder = SignatureBuilder.Builder()
-            .oauthConsumerKey(consumerKey)
-            .oauthConsumerSecret(consumerSecret)
-            .build()
-
-        val authorizationHeaderString =
-            buildSignature(
-                "POST",
-                signatureBuilder,
-                "oauth/request_token",
-                emptyMap()
-            )
-
         builder.url("$BASEURL/oauth/request_token")
-        builder.headers.append(HttpHeaders.Authorization, authorizationHeaderString)
+        builder.headers.append(
+            HttpHeaders.Authorization,
+            createAuthorizationHeaderString(consumerKey, consumerSecret, "POST", "/oauth/request_token")
+        )
+        builder.method = HttpMethod.Post
 
-        val response: HttpResponse = client.post(builder)
-
+        val response: HttpResponse = client.execute(builder)
         val stringBody: String = response.body()
+
         client.close()
+
         return RequestToken(
-            stringBody.substring(
-                stringBody.indexOf("oauth_token=") + TWELVE,
-                stringBody.indexOf("&oauth_token_secret")
-            ),
-            stringBody.substring(
-                stringBody.indexOf("&oauth_token_secret=") + TWENTY,
-                stringBody.indexOf("&oauth_callback_confirmed")
-            ),
-            stringBody.substring(stringBody.indexOf("&oauth_callback_confirmed=") + TWENTYSIX)
+            getTokenValue(stringBody, "oauth_token="),
+            getTokenValue(stringBody, "&oauth_token_secret="),
+            getTokenValue(stringBody, "&oauth_callback_confirmed=")
         )
     }
 
@@ -60,39 +42,45 @@ class TwitterAuthentication {
         authToken: String,
         authVerifier: String
     ): AccessToken {
-        val client = ClientConfig()
         val builder = HttpRequestBuilder()
+        builder.url("$BASEURL/oauth/access_token?oauth_token=$authToken&oauth_verifier=$authVerifier")
+        builder.headers.append(
+            HttpHeaders.Authorization,
+            createAuthorizationHeaderString(consumerKey, consumerSecret, "POST", "/oauth/access_token")
+        )
+        builder.method = HttpMethod.Post
 
+        val response: HttpResponse = client.execute(builder)
+        val stringBody: String = response.body()
+
+        client.close()
+
+        return AccessToken(
+            getTokenValue(stringBody, "oauth_token="),
+            getTokenValue(stringBody, "&oauth_token_secret="),
+            getTokenValue(stringBody, "&user_id="),
+            getTokenValue(stringBody, "&screen_name=")
+        )
+    }
+
+    private fun createAuthorizationHeaderString(
+        consumerKey: String,
+        consumerSecret: String,
+        method: String,
+        endpoint: String
+    ): String {
         val signatureBuilder = SignatureBuilder.Builder()
             .oauthConsumerKey(consumerKey)
             .oauthConsumerSecret(consumerSecret)
             .build()
 
-        val authorizationHeaderString = buildSignature(
-            "POST",
-            signatureBuilder,
-            "/oauth/access_token?oauth_token=",
-            emptyMap()
+        return buildSignature(method, signatureBuilder, endpoint, emptyMap())
+    }
+
+    private fun getTokenValue(stringBody: String, key: String): String {
+        return stringBody.substring(
+            stringBody.indexOf(key) + key.length,
+            stringBody.indexOf("&", startIndex = stringBody.indexOf(key) + key.length)
         )
-
-        builder.url("$BASEURL/oauth/access_token?oauth_token=$authToken&oauth_verifier=$authVerifier")
-        builder.headers.append(HttpHeaders.Authorization, authorizationHeaderString)
-
-        val response: HttpResponse = client.post(builder)
-
-        val stringBody: String = response.body()
-        val authTokenString = stringBody.substring(
-            stringBody.indexOf("oauth_token=") + TWELVE,
-            stringBody.indexOf("&oauth_token_secret=")
-        )
-        val authSecret = stringBody.substring(
-            stringBody.indexOf("&oauth_token_secret=") + TWENTY,
-            stringBody.indexOf("&user_id")
-        )
-        val userId = stringBody.substring(stringBody.indexOf("&user_id=") + NINE, stringBody.indexOf("&screen_name"))
-        val userName = stringBody.substring(stringBody.indexOf("&screen_name=") + THIRTEEN)
-        client.close()
-
-        return AccessToken(authTokenString, authSecret, userId, userName)
     }
 }
