444746e5052d88b6bc0624a5207f5cd22be1623f
Thomas Carney <tpctoy@gmail.com>
2023-03-14 22:15:06 -0400

    Change the CountTweetsAPI class constructor to take a ClientConfig object as a parameter. Update the recent method to use HttpRequestBuilderWrapper to build the HTTP request. Update the paramMap parameter processing in the recent method to use the url.encodedParameters.append function instead of builder.url.parameters.append. Add content type and authorization headers to the HttpRequestBuilderWrapper object used in the recent method. The purpose of these changes is to refactor the CountTweetsAPI class to use the HttpRequestBuilderWrapper class instead of the HttpRequestBuilder class, which provides a more convenient and safer way to build HTTP requests.

--- src/main/kotlin/com/chromasgaming/ktweet/api/CountTweetsAPI.kt
+++ src/main/kotlin/com/chromasgaming/ktweet/api/CountTweetsAPI.kt
@@ -5,19 +5,20 @@ import com.chromasgaming.ktweet.constants.BASEURL
 import com.chromasgaming.ktweet.constants.VERSION
 import com.chromasgaming.ktweet.models.BearerToken
 import com.chromasgaming.ktweet.models.TweetCount
+import com.chromasgaming.ktweet.util.HttpRequestBuilderWrapper
 import io.ktor.client.call.body
-import io.ktor.client.request.HttpRequestBuilder
-import io.ktor.client.request.url
+import io.ktor.client.request.headers
 import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
-import kotlinx.coroutines.runBlocking
+import io.ktor.http.append
 
 /**
  * This class handles all API calls that exist under Tweet Counts.
  * API Reference [Twitter API](https://developer.twitter.com/en/docs/twitter-api/tweets/counts/api-reference).
  */
-class CountTweetsAPI {
+class CountTweetsAPI(private val client: ClientConfig) {
 
     /**
      * Receive a count of Tweets [TweetCount] that match a query in the last 7 days
@@ -25,22 +26,22 @@ class CountTweetsAPI {
      */
     suspend fun recent(paramMap: LinkedHashMap<String, String>, bearerToken: BearerToken): TweetCount {
         var response: HttpResponse
-        runBlocking {
-            val client = ClientConfig()
-            val builder = HttpRequestBuilder()
-            builder.method = HttpMethod.Get
-            builder.url("$BASEURL/$VERSION/tweets/counts/recent")
 
+        val builder = HttpRequestBuilderWrapper("$BASEURL/$VERSION/tweets/counts/recent") {
+            method = HttpMethod.Get
             paramMap.forEach { (key, value) ->
-                builder.url.parameters.append(key, value)
+                url.encodedParameters.append(key, value)
             }
+            headers {
+                append(HttpHeaders.ContentType, ContentType.Application.Json)
+                append(HttpHeaders.Authorization, "Bearer ${bearerToken.access_token}")
+            }
+        }.build()
 
-            builder.headers.append(HttpHeaders.Authorization, "Bearer ${bearerToken.access_token}")
-            builder.headers.append(HttpHeaders.ContentType, "application/json")
-            response = client.execute(builder)
+        response = client.execute(builder)
 
-            client.close()
-        }
+        client.close()
+
 
         return response.body()
     }
