b44c4d3903e3d818f4809ee4104e9a10f1fccb96
Thomas Carney <tpctoy@gmail.com>
2023-03-17 04:48:29 -0400

    "Improve SignatureBuilder and buildSignature function in OAuth module
    Change constructor parameters to non-nullable types
    Replace string concatenation with list-based joinToString
    Simplify additionalParameters generation
    Include paramMap entries in oauthBuilder string
    Update return statement format for buildSignature function"

--- src/main/kotlin/com/chromasgaming/ktweet/oauth/SignatureBuilder.kt
+++ src/main/kotlin/com/chromasgaming/ktweet/oauth/SignatureBuilder.kt
@@ -13,17 +13,17 @@ import javax.crypto.spec.SecretKeySpec
  * Reference [Authentication](https://developer.twitter.com/en/docs/authentication/oauth-1-0a/creating-a-signature)
  */
 class SignatureBuilder private constructor(
-    val oauthConsumerKey: String?,
-    val oauthConsumerSecret: String?,
-    val accessToken: String?,
-    val accessTokenSecret: String?
+    val oauthConsumerKey: String,
+    val oauthConsumerSecret: String,
+    val accessToken: String,
+    val accessTokenSecret: String
 ) {
 
     data class Builder(
-        var oauthConsumerKey: String? = null,
-        var oauthConsumerSecret: String? = null,
-        var accessToken: String? = null,
-        var accessTokenSecret: String? = null
+        private var oauthConsumerKey: String = "",
+        private var oauthConsumerSecret: String = "",
+        private var accessToken: String = "",
+        private var accessTokenSecret: String = ""
     ) {
         fun oauthConsumerKey(oauthConsumerKey: String) = apply { this.oauthConsumerKey = oauthConsumerKey }
         fun oauthConsumerSecret(oauthConsumerSecret: String) = apply { this.oauthConsumerSecret = oauthConsumerSecret }
@@ -46,59 +46,43 @@ fun buildSignature(
     val oauthTimestamp = (System.currentTimeMillis() / MILLISECONDS).toString()
     val oauthVersion = "1.0"
 
-    val additionalParameters = StringBuilder()
-    val accessToken = signatureBuilder.accessToken
-    val oauthConsumerKey = signatureBuilder.oauthConsumerKey
-    val oauthConsumerSecret = signatureBuilder.oauthConsumerSecret
-    val accessTokenSecret = signatureBuilder.accessTokenSecret
-
-    if (accessToken.isNullOrBlank()) {
-        additionalParameters.append(HttpAuthHeader.Parameters.OAuthVersion + "=$oauthVersion")
-    } else {
-        additionalParameters.append(HttpAuthHeader.Parameters.OAuthToken + "=$accessToken&")
-        additionalParameters.append(HttpAuthHeader.Parameters.OAuthVersion + "=$oauthVersion")
-    }
-
-    if (paramMap.isNotEmpty()) {
-        additionalParameters.append("&")
-        var count = 1
-        paramMap.forEach { (key, value) ->
-            additionalParameters.append("$key=$value")
-            if (paramMap.size > 1 && paramMap.size != count) {
-                additionalParameters.append("&")
-                count += 1
-            }
+    val additionalParameters = mutableListOf<String>().apply {
+        if (signatureBuilder.accessToken.isNotEmpty()) {
+            add("${HttpAuthHeader.Parameters.OAuthToken}=${signatureBuilder.accessToken}")
         }
-
+        add("${HttpAuthHeader.Parameters.OAuthVersion}=$oauthVersion")
     }
 
-    val oauthBuilder =
-        HttpAuthHeader.Parameters.OAuthConsumerKey + "=$oauthConsumerKey&" +
-                HttpAuthHeader.Parameters.OAuthNonce + "=$oauthNonce&" +
-                HttpAuthHeader.Parameters.OAuthSignatureMethod + "=$oauthSignatureMethod&" +
-                HttpAuthHeader.Parameters.OAuthTimestamp + "=$oauthTimestamp&" +
-                additionalParameters.toString()
+    val oauthBuilder = (
+            listOf(
+                "${HttpAuthHeader.Parameters.OAuthConsumerKey}=${signatureBuilder.oauthConsumerKey}",
+                "${HttpAuthHeader.Parameters.OAuthNonce}=$oauthNonce",
+                "${HttpAuthHeader.Parameters.OAuthSignatureMethod}=$oauthSignatureMethod",
+                "${HttpAuthHeader.Parameters.OAuthTimestamp}=$oauthTimestamp"
+            ) + additionalParameters + paramMap.map { "${it.key}=${it.value}" }
+            ).joinToString("&")
+
 
     val urlEncodedOAuthString = URLEncoder.encode(oauthBuilder, "UTF-8")
     val requestTokenApi = URLEncoder.encode("$BASEURL/$path", "UTF-8")
     val baseString = "$requestMethod&$requestTokenApi&$urlEncodedOAuthString"
+
     val oauthSig =
         createSignatureBase64(
             baseString,
-            "$oauthConsumerSecret&" + if (accessTokenSecret.isNullOrBlank()) "" else accessTokenSecret
+            "${signatureBuilder.oauthConsumerSecret}&${signatureBuilder.accessTokenSecret}"
         )
     val urlEncodedOAuthSig = URLEncoder.encode(oauthSig, "UTF-8")
 
-    return "OAuth ${HttpAuthHeader.Parameters.OAuthConsumerKey}=${'"'}$oauthConsumerKey${'"'}" +
-            ",${HttpAuthHeader.Parameters.OAuthSignatureMethod}=${'"'}$oauthSignatureMethod${'"'}" +
-            ",${HttpAuthHeader.Parameters.OAuthTimestamp}=${'"'}$oauthTimestamp${'"'}" +
-            ",${HttpAuthHeader.Parameters.OAuthNonce}=${'"'}$oauthNonce${'"'}" +
-            ",${HttpAuthHeader.Parameters.OAuthSignature}=${'"'}$urlEncodedOAuthSig${'"'}" +
-            if (accessToken.isNullOrBlank()) ",${HttpAuthHeader.Parameters.OAuthVersion}=${'"'}$oauthVersion${'"'}"
-            else
-                ",${HttpAuthHeader.Parameters.OAuthToken}=${'"'}$accessToken${'"'}" +
-                        ",${HttpAuthHeader.Parameters.OAuthVersion}=${'"'}$oauthVersion${'"'}"
-
+    return "OAuth " + (
+            listOf(
+                "${HttpAuthHeader.Parameters.OAuthConsumerKey}=\"${signatureBuilder.oauthConsumerKey}\"",
+                "${HttpAuthHeader.Parameters.OAuthSignatureMethod}=\"$oauthSignatureMethod\"",
+                "${HttpAuthHeader.Parameters.OAuthTimestamp}=\"$oauthTimestamp\"",
+                "${HttpAuthHeader.Parameters.OAuthNonce}=\"$oauthNonce\"",
+                "${HttpAuthHeader.Parameters.OAuthSignature}=\"$urlEncodedOAuthSig\""
+            ) + additionalParameters.map { "${it.substringBefore('=')}=\"${it.substringAfter('=')}\"" }
+            ).joinToString(",")
 }
 
 private fun createSignatureBase64(data: String, key: String): String {
@@ -107,4 +91,5 @@ private fun createSignatureBase64(data: String, key: String): String {
     mac.init(secretKey)
 
     return Base64.getEncoder().encodeToString(mac.doFinal(data.toByteArray())).trim()
+
 }
